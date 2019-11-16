bfa19b5e4760cb9511ae5088c8ee8834a016ed29
Z94me <58160323@go.buu.ac.th>
2019-11-16 16:24:12 +0700

    Create button for change text

--- TrainingGit/ViewController.swift
+++ TrainingGit/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textlable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnChangeText(_ sender: Any) {
+        textlable.text = "Why you navigation to next page"
+    }
+    
 }
 
