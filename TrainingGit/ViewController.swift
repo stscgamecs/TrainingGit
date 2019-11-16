c4306c39a2e5d95fe2e42fd4d04ddb60a4f89891
Z94me <58160323@go.buu.ac.th>
2019-11-16 16:11:22 +0700

    Start Project

---
+++ TrainingGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainingGit
+//
+//  Created by Z94me on 16/11/2562 BE.
+//  Copyright © 2562 Z94me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
